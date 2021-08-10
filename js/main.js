edf5b8700f9a0d8af8fa317f657d6b80be619207
Nicu20 <leontenicu01@gmail.com>
2021-08-10 20:55:43 +0300

    media quaries menu section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ function closePopup(){
     document.getElementById("open").style.display = "none";
 }
 
+function showMenu(){
+    document.getElementById("show-close-menu").style.display = "block";
+}
+
+function closeMenu(){
+    if(document.getElementById("toggler") === false){
+       document.getElementById("show-close-menu").style.display = "none";
+    }
+}
+
